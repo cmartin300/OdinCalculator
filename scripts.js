fd77719021b13ac8f90dcde4356baed623466538
Lobstaa <martin.christophercharles@gmail.com>
2020-02-20 15:43:25 -0700

    Funtionality updates, still need to string operations

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
-function subtract() {
-    let result;
+let displayValue = '0';
+let lastValue = '0';
+let operation;
+const displayText = document.querySelector(".display");
 
-    return result;
+
+
+/*Math Functions*/
+function subtract(num1, num2) {
+    return num1 - num2;
 }
 
 function add(num1, num2) {
@@ -14,4 +20,75 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+
+
+/*Display Screen Functions*/
+function clearScreen(value) {
+    displayValue = '0';
+    lastValue = '0';
+    operation = '';
+    displayText.textContent = displayValue
+}
+
+function changeDisplay(value) {
+    displayValue = `${value}`;
+    displayText.textContent = displayValue;
+}
+
+function updateDisplay (value) {
+    if (displayValue == 0) {
+        changeDisplay(value);
+    } else {
+        if (value == '.') {
+            if (!displayValue.includes(".")) {
+                displayValue += `${value}`;
+            }
+        } else {
+            displayValue += `${value}`;
+        }
+        changeDisplay(displayValue);
+    }
+}
+
+
+
+/*Operation Functions */
+function updateOperation (operator) {
+    lastValue = displayValue;
+    displayValue = '0';
+    operation = operator;
+}
+
+function operate(operator, num1, num2) {
+    let result;
+
+    if (!operator) {
+        result = displayValue;
+    }
+    if (operator == '+') {
+        let newNum1 = Number(num1);
+        let newNum2 = Number(num2);
+        result = add(newNum1, newNum2);
+    }
+    else if (operator == '-') {
+        result = subtract(num1, num2);
+    }
+    else if (operator == 'X') {
+        result = multiply(num1, num2);
+    }
+    else if (operator == '/') {
+        if (num2 != 0) {
+            result = divide(num1, num2);
+        } else {
+            result = 'ERROR!';
+        }
+    }
+    changeDisplay(result);
+    if (result == 'ERROR!') {
+        displayValue = '0'
+        lastValue = '0';
+        operation = '';
+    }
 }
